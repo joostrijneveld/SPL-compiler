ccc58806b1a1794097e942c06560ee3a752248b0
Koen van Ingen <koenvaningen@gmail.com>
2014-03-11 18:09:13 +0100

    Added an usage option to toolchain.py

--- src/toolchain.py
+++ src/toolchain.py
@@ -3,7 +3,13 @@
 import sys
 import parser, scanner, prettyprinter
 
-def main():
+def help():
+        print "Usage: toolchain.py inputfile.spl"
+
+def main(): 
+        if len(sys.argv) != 2:
+            help()
+            return
 	tokens = scanner.scan_spl(sys.argv[1])
 	tree = parser.build_tree(tokens)
 	print tree
