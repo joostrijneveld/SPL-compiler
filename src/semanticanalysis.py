8b6599c953cabee96935207f6189bacda23e9c0f
Joost Rijneveld <joostrijneveld@gmail.com>
2014-04-01 13:27:26 +0200

    Started typechecking

--- src/semanticanalysis.py
+++ src/semanticanalysis.py
@@ -1,9 +1,20 @@
 #! /usr/bin/env python
 
 import collections
+import sys
+from functools import partial
 
 Symbol = collections.namedtuple('Symbol', ['line','col','type','argtype'])
 
+class Type:
+	def __init__(self, value):
+		self.value = value
+	
+	def from_node(tree):
+		pass
+	def __eq__(self, other):
+		pass	
+		
 def print_symboltable(symboltable):
 	print '='*62
 	print ("{0: <12} {1: <15} {2: <15} {3: <20}"
@@ -21,7 +32,18 @@ def find_argtypes(tree):
 		return []
 	return [tree.children[0]] + find_argtypes(tree.children[2])
 
+def update_symbols(symbols, sym, t, argtypes):
+	if sym.val in symbols:
+		dupsym = symbols[sym.val]
+		raise Exception("[Line {}:{}] Redefinition of {}\n"+
+						"[Line {}:{}] Previous definition was here"
+						.format(dupsym.line, dupsym.col, sym.val,
+								sym.line, sym.col))
+	symbols.update({sym.val : Symbol(sym.line, sym.col, t, argtypes)})
+	return symbols
+
 def create_table(tree):
+	''' expects a tree with a Decl-node as root '''
 	if not tree:
 		return dict()
 	t = tree.children[0].children[0]
@@ -30,14 +52,7 @@ def create_table(tree):
 	if tree.children[0].tok == 'FunDecl':
 		argtypes = find_argtypes(tree.children[0].children[2])
 	symbols = create_table(tree.children[1])
-	if sym.val in symbols:
-		dupsym = symbols[sym.val]
-		raise Exception("[Line {}:{}] Redefinition of {}\n"+
-						"[Line {}:{}] Previous definition was here"
-						.format(dupsym.line, dupsym.col, sym.val,
-								sym.line, sym.col))
-	symbols.update({sym.val : Symbol(sym.line, sym.col, t, argtypes)})
-	return symbols
+	return update_symbols(symbols, sym, t, argtypes)
 
 def create_argtable(tree):
 	''' expects a tree with an arg-node (',') as root '''
@@ -46,17 +61,9 @@ def create_argtable(tree):
 	t = tree.children[0]
 	sym = tree.children[1].tok
 	symbols = create_argtable(tree.children[2])
-	argtypes = None  # VarDecls do not have arguments
-	if sym.val in symbols:
-		dupsym = symbols[sym.val]
-		raise Exception("[Line {}:{}] Redefinition of {}\n"+
-						"[Line {}:{}] Previous definition was here"
-						.format(dupsym.line, dupsym.col, sym.val,
-								sym.line, sym.col))
-	symbols.update({sym.val : Symbol(sym.line, sym.col, t, argtypes)})
-	return symbols
+	return update_symbols(symbols, sym, t, None)
 
-def check_functionbinding(tree, symboltable):
+def create_functiontable(tree):
 	argsymboltable = create_argtable(tree.children[2])
 	localsymboltable = create_table(tree.children[3])
 	for key in set(localsymboltable.keys()) & set(argsymboltable.keys()):
@@ -66,15 +73,91 @@ def check_functionbinding(tree, symboltable):
 						.format(dupsym.line, dupsym.col, sym.val,
 								sym.line, sym.col))
 	localsymboltable.update(argsymboltable)
-	print_symboltable(localsymboltable)
-	# TODO: check binding
+	return localsymboltable
 
-def check_localbinding(tree, symboltable):
+def type_op2(fn, tree, symtab, expected_type):
+	if tree.tok.type == '||':
+		if type_exp(tree.children[0]) != expected_type or
+			type_exp(tree.children[1]) != expected_type:
+			raise Exception("[Line {}:{}] Incompatible types for operator {}\n"
+							+ "Types found: {} {}"
+							.format(tree.tok.line, tree.tok.col, tree.tok.type
+								t1, t2))
+		return expected_type
+	return fn(tree, symtab)
+
+type_exp_or = partial(type_op2, type_exp_and, expected_type='Bool')
+type_exp = type_exp_or
+
+def type_expargs(tree, symtab):
+	''' expects a tree with an arg-node (',') as root '''
+	if not tree:
+		return []
+	return ([type_exp(tree.children[0], symtab)]
+			+ type_expargs(tree.children[1].symtab))
+
+def type_expfunc(tree, symtab):
+	return symtab[tree.children[0].tok.val].type
+
+def check_stmt(tree, symboltable):	
+	''' expects a tree with a statement node as root '''
+	print tree
+	if tree.tok == 'Scope':
+		check_stmts(tree.children[0], symboltable)
+	elif tree.tok == 'FunCall':
+		out(tree.children[0].tok.val, depth)
+		out('(')
+		if tree.children[1]:
+			print_act_args(tree.children[1])
+		out(');\n')
+	elif tree.tok.type == 'if':
+		out('if (', depth)
+		print_exp(tree.children[0])
+		out(')\n')
+		print_stmt(tree.children[1], depth + int(tree.children[1].tok != 'Scope'))
+		if tree.children[2]:
+			out('else\n', depth)
+			print_stmt(tree.children[2], depth + int(tree.children[2].tok != 'Scope'))
+	elif tree.tok.type == 'while':
+		out('while (', depth)
+		print_exp(tree.children[0])
+		out(')\n')
+		print_stmt(tree.children[1], depth + int(tree.children[1].tok != 'Scope'))
+	elif tree.tok.type == '=':
+		out('', depth)
+		print_field(tree.children[0])
+		out(' = ')
+		print_exp(tree.children[1])
+		out(';\n')
+	elif tree.tok.type == 'return':
+		out('return', depth)
+		if tree.children[0]:
+			out(' ')
+			print_exp(tree.children[0])
+		out(';\n')
+	
+def check_stmts(tree, symboltable):
+	if tree:
+		check_stmt(tree.children[0], symboltable)
+		check_stmts(tree.children[1], symboltable)
+
+def check_functionbinding(tree, globalsymboltable):
+	functionsymboltable = create_functiontable(tree)
+	for key in set(functionsymboltable.keys()) & set(globalsymboltable.keys()):
+		dupsym = symbols[key]
+		sys.stderr.write("[Line {}:{}] Warning: redefinition of global {}\n"+
+						"[Line {}:{}] Previous definition was here"
+						.format(dupsym.line, dupsym.col, sym.val,
+								sym.line, sym.col))
+	symboltable = globalsymboltable.copy().update(functionsymboltable)
+	check_stmts(tree.children[4], symboltable)
+
+def check_localbinding(tree, globalsymboltable):
 	if not tree:
 		return
 	if tree.children[0].tok == 'FunDecl':
-		check_functionbinding(tree.children[0], symboltable)
-	check_localbinding(tree.children[1], symboltable)
+		check_functionbinding(tree.children[0], globalsymboltable)
+	check_localbinding(tree.children[1], globalsymboltable)
 
 def check_binding(tree):
 	globalsymboltable = create_table(tree)
