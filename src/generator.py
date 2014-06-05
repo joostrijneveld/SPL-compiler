joostrijneveld/SPL-compiler

commit 928fc02ad8fc626870c6810e89474eb30221c217

Added Type equality to check for Void returntype, removed unnecessary check in gen_exp

src/generator.py
@@ -40,8 +40,6 @@ def gen_exp_base(tree, wab, tables):
     elif tree.tok.type == 'FunCall':
         rettype = tables[tree.children[0].tok.val].type
         result = gen_funcall(tree, wab, tables)
-        if rettype != Type('Void'):
-            result += ['ldr RR']
         return result
     elif tree.tok.type == ',':
         result = (gen_exp(tree.children[0], wab, tables) +

src/semanticanalysis.py
@@ -28,6 +28,9 @@ class Type(object):
 
     def __repr__(self):
         return repr(self.value)
+
+    def __eq__(self, other):
+        return self.value == other.value
         
     def unify(self, other):
         ''' attempts to unify non-generic types (necessary for empty lists)

commit 9588995f07ca8b0326e73e88f26a619b950e6604

Fix bug introduced in 928fc02ad8fc626870c6810e89474eb30221c217; str RR is necessary

src/generator.py
@@ -38,9 +38,7 @@ def gen_exp_base(tree, wab, tables):
     elif tree.tok.type == '[]':
         return ['ldc 0']
     elif tree.tok.type == 'FunCall':
-        rettype = tables[tree.children[0].tok.val].type
-        result = gen_funcall(tree, wab, tables)
-        return result
+        return gen_funcall(tree, wab, tables) + ['ldr RR']
     elif tree.tok.type == ',':
         result = (gen_exp(tree.children[0], wab, tables) +
                   gen_exp(tree.children[1], wab, tables))