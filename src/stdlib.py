79c29feccc6b9c3c14fa73e8dd9a23b72c5f5fa6
Joost Rijneveld <joostrijneveld@gmail.com>
2014-06-05 14:05:18 +0200

    Added SPL builtins for reading user input and typecasting ints and chars

--- src/stdlib.py
+++ src/stdlib.py
@@ -3,10 +3,23 @@
 from semanticanalysis import Type, Symbol
 
 functions = {
+	'toInt': (Symbol(0, 0, Type('Int'), [Type('t')], True, None),
+		['ldl 1']),
+	'toChar': (Symbol(0, 0, Type('Char'), [Type('t')], True, None),
+		['ldl 1']),
 	'isEmpty': (Symbol(0, 0, Type('Bool'), [Type([Type('t')])], True, None),
 		['ldl 1', 'ldc 0', 'eq']),
 	'print': (Symbol(0, 0, Type('Void'), [Type('t')], True, None),
 		['ldl 1', 'trap 0']),
 	'printChar': (Symbol(0, 0, Type('Void'), [Type('Char')], True, None),
-		['ldl 1', 'trap 1'])
+		['ldl 1', 'trap 1']),
+	'readInt': (Symbol(0, 0, Type('Int'), [], True, None),
+		['trap 2']),
+	'readChar': (Symbol(0, 0, Type('Char'), [], True, None),
+		['trap 3']),
+	'readCharList': (Symbol(0, 0, Type([Type('Char')]), [], True, None),
+		['trap 4', 'brf endemptylist', 'ajs 1', 'ldc 0', 'loop:', 'sth',
+		'ajs -1', 'sth', 'ajs -1', 'brf endloop', 'ajs 2', 'bra loop',
+		'endloop:', 'ajs 2', 'str RR', 'ajs -1', 'unlink', 'ret',
+		'endemptylist:', 'ldc 0'])  # str RR, unlink and ret added by generator
 }
